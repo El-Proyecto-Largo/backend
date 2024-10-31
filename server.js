26c202c152cfbb6367f178384ef1dedadb54ea07
Bed Pandey <bed.dev.p@gmail.com>
2024-10-31 14:18:55 -0400

    i forgo

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const e = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb');
 require('dotenv').config()
 
 const url = process.env.DB_URL;
@@ -13,16 +14,23 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-
 app.post('/api/makepost', async (req, res, next) => {
   // incoming: title, body, image, latitude, longitude, authorId, tags
   // outgoing: error
 
-  const { title, body, image, latitude, authorId, longitude, tags } = req.body;
+  const {title, body, image, latitude, authorId, longitude, tags} = req.body;
 
-  const newPost = { title: title, body: body, image: image, latitude: latitude, authorId: authorId, longitude: longitude, tags: tags };
-  var error = '';
+  const newPost = {
+    title: title, 
+    body: body, 
+    image: image, 
+    latitude: latitude, 
+    authorId: authorId, 
+    longitude: longitude, 
+    tags: tags 
+  };
+  
+  let error = '';
 
   try
   {
@@ -36,7 +44,7 @@ app.post('/api/makepost', async (req, res, next) => {
 
   // add catch for format mismatch
 
-  var ret = { error: error };
+  let ret = { error: error };
   res.status(200).json(ret);
 });
 
@@ -46,12 +54,12 @@ app.post('/api/searchposts', async (req, res, next) => {
   // outgoing: title, body, image, latitude, longitude, authorId, tags
   // Partial matching w/ regex
 
-  var error = '';
+  let error = '';
 
   const { title, body, authorId, tags } = req.body;
 
   const db = client.db('Overcastly');
-  var results = [];
+  let results = [];
   const resultsBody = await db.collection('Posts').find({ $or: [{title: { $regex: title.trim() + '.*', $options: 'i' }},
               {body: { $regex: body.trim() + '.*', $options: 'i' }}, { authorId : authorId }] }).toArray();
 
@@ -66,15 +74,15 @@ app.post('/api/searchposts', async (req, res, next) => {
     results = resultsBody;
   }
 
-  var outId = -1;
-  var outTitle = '';
-  var outBody = '';
-  var outImage = -1;
-  var outLat = -1;
-  var outLong = -1;
-  var outTags = -1;
+  let outId = -1;
+  let outTitle = '';
+  let outBody = '';
+  let outImage = -1;
+  let outLat = -1;
+  let outLong = -1;
+  let outTags = -1;
 
-  var ret = [];
+  let ret = [];
 
   for (let i = 0; i < results.length; i++)
     {
@@ -86,13 +94,16 @@ app.post('/api/searchposts', async (req, res, next) => {
       outLong = results[i].longitude;
       outTags = results[i].tags;
   
-      ret.push({ title: outTitle, body: outBody, image: outImage, latitude: outLat, authorId: outId, longitude: outLong, tags: outTags, error: '' });
+      ret.push({ 
+        title: outTitle, 
+        body: outBody, 
+        image: outImage, 
+        latitude: outLat, 
+        authorId: outId, 
+        longitude: outLong, 
+        tags: outTags, 
+        error: '' });
     }
-
-  if (results.length == 0)
-  {
-    var ret = { error: 'No matching posts found' };
-  }
   
   res.status(200).json(ret);
 });
@@ -102,26 +113,26 @@ app.post('/api/findlocalposts', async (req, res, next) => {
   // outgoing: title, body, image, latitude, longitude, authorId, tags
   // returns array of posts within distance of latitude and longitude
 
-  var error = '';
+  let error = '';
 
   const { latitude, longitude, distance } = req.body;
 
   const db = client.db('Overcastly');
   const results = await db.collection('Posts').find({ }).toArray();
 
-  var outId = -1;
-  var outTitle = '';
-  var outBody = '';
-  var outImage = -1;
-  var outLat = -1;
-  var outLong = -1;
-  var outTags = -1;
+  let outId = -1;
+  let outTitle = '';
+  let outBody = '';
+  let outImage = -1;
+  let outLat = -1;
+  let outLong = -1;
+  let outTags = -1;
 
-  var ret = [];
+  let ret = [];
 
   for (let i = 0; i < results.length; i++)
   {
-    var calcDistance = Math.sqrt( (results[i].latitude - latitude)**2 + (results[i].longitude - longitude)**2 )
+    let calcDistance = Math.sqrt( (results[i].latitude - latitude)**2 + (results[i].longitude - longitude)**2 )
 
     if (calcDistance > distance || !(results[i].hasOwnProperty('latitude') &&  results[i].hasOwnProperty('longitude')))
       continue;
@@ -134,68 +145,11 @@ app.post('/api/findlocalposts', async (req, res, next) => {
     outLong = results[i].longitude;
     outTags = results[i].tags;
 
-    ret.push({ title: outTitle, body: outBody, image: outImage, latitude: outLat, authorId: outId, longitude: outLong, tags: outTags, error: '' });
-  }
-
-  if (ret.length == 0)
-  {
-    var ret = { error: 'No matching posts found' }
+    ret.push({ title: outTitle, body: outBody, image: outImage, latitude: outLat, authorId: outId, longitude: outLong, tags: outTags });
   }
   
   res.status(200).json(ret);
 });
-
-
-
-
-app.post('/api/login', async (req, res, next) => {
-  // incoming: login, password
-  // outgoing: id, firstName, lastName, error
-
-  var error = '';
-
-  const { login, password } = req.body;
-
-  const db = client.db('COP4331Cards');
-  const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
-
-  var id = -1;
-  var fn = '';
-  var ln = '';
-
-  if (results.length > 0) {
-    id = results[0].UserId;
-    fn = results[0].FirstName;
-    ln = results[0].LastName;
-  }
-
-  var ret = { id: id, firstName: fn, lastName: ln, error: '' };
-  res.status(200).json(ret);
-});
-
-
-app.post('/api/searchcards', async (req, res, next) => {
-  // incoming: userId, search
-  // outgoing: results[], error
-
-  var error = '';
-
-  const { userId, search } = req.body;
-
-  var _search = search.trim();
-
-  const db = client.db('COP4331Cards');
-  const results = await db.collection('Cards').find({ "Card": { $regex: _search + '.*', $options: 'i' } }).toArray();
-
-  var _ret = [];
-  for (var i = 0; i < results.length; i++) {
-    _ret.push(results[i].Card);
-  }
-
-  var ret = { results: _ret, error: error };
-  res.status(200).json(ret);
-});
-
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
