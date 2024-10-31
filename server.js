0fb4f7ee12c7d83a6f5f129238052c1be869a9c7
pedicino <pablo.pedicino@icloud.com>
2024-10-31 16:34:34 -0400

    Added user CRUD endpoints
    Added the following endpoints:
    - Register user
    - Search user by Id
    - Update user
    - Delete user

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const bcrypt = require("bcryptjs");
+const { ObjectId } = require("mongodb");
+
 app.post('/api/makepost', async (req, res, next) => {
   // incoming: title, body, image, latitude, longitude, authorId, tags
   // outgoing: error
@@ -47,7 +50,6 @@ app.post('/api/makepost', async (req, res, next) => {
   let ret = { error: error };
   res.status(200).json(ret);
 });
-
 
 app.post('/api/searchposts', async (req, res, next) => {
   // incoming: title, body, authorId, tags
@@ -149,6 +151,150 @@ app.post('/api/findlocalposts', async (req, res, next) => {
   }
   
   res.status(200).json(ret);
+});
+
+
+
+// User registration
+app.post("/api/registeruser", async (req, res, next) => {
+  // incoming: username, password, firstName, lastName, email
+  // outgoing: error
+
+  const { username, password, firstName, lastName, email } = req.body;
+
+  if (!username || !password || !firstName || !lastName || !email) {
+    return res.status(400).json({ error: "Missing some registerfields :((" });
+  }
+
+  try {
+    const db = client.db("Overcastly");
+
+    const existingUser = await db.collection("Users").findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (existingUser) {
+      const error = existingUser.username === username
+        ? "username already exists!"
+        : "email has already been registered to an account......";
+
+      return res.status(409).json({ error });
+    }
+
+    const hashedPasswd = await bcrypt.hash(password, 10);
+    
+    const newUser = {
+      username,
+      password: hashedPasswd,
+      firstName,
+      lastName,
+      email,
+    };
+
+    await db.collection("Users").insertOne(newUser);
+
+    return res.status(201).json({ message: "User registered successfully >W<" });
+
+  } catch (e) {
+    return res.status(500).json({ error: "A servar ewwow happend ;(" });
+  }
+});
+
+// Retrieve user by Id
+app.get("/api/users/:_id", async (req, res, next) => {
+  // incoming: user Id
+  // outgoing: user info
+
+  try {
+    const db = client.db("Overcastly");
+    let _id = req.params._id;
+
+    let readUser = await db.collection("Users").findOne({ _id: new ObjectId(_id) });
+
+    if (!readUser) {
+      return res.status(404).json({ error: "User not found :(" });
+    }
+
+    delete readUser.password;
+
+    return res.status(200).json(readUser);
+
+  } catch (e) {
+    return res.status(500).json({ error: "couldnt fetch user details wtf!!" });
+  }
+});
+
+// Update user by Id
+app.put("/api/updateuser/:_id", async (req, res, next) => {
+  // incoming: new user data
+  // outgoing: success or error
+
+  try {
+    const db = client.db("Overcastly");
+    let _id = req.params._id;
+
+    const { firstName, lastName, email } = req.body;
+
+    if (!firstName && !lastName && !email) {
+      return res.status(400).json({ error: "you need to provide a field to update bruh" });
+    }
+
+    if (email && !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+      return res.status(400).json({ error: "bro yo email format fricked up" });
+    }
+
+    let readUser = await db.collection("Users").findOne(_id.ObjectId);
+
+    if (!readUser) {
+      return res.status(404).json({ error: "User not found :(" });
+    }
+
+    const updatedFields = {};
+
+    if (firstName) {
+      updatedFields.firstName = firstName;
+    }
+
+    if (lastName) {
+      updatedFields.lastName = lastName;
+    }
+
+    if (email) {
+      updatedFields.email = email;
+    }
+
+    const result = await db.collection("Users").updateOne(
+      { _id: new ObjectId(_id) }, // searching for a specific id syntax
+      { $set: updatedFields }
+    );
+
+    return res.status(200).json({ message: "updated dat user x3" });
+
+  } catch (error) {
+    return res.status(500).json({ error: "couldnt update that user wtf hapepnd" });
+  }
+});
+
+// Delete user by id
+app.delete("/api/deleteuser/:_id", async (req, res, next) => {
+  // incoming: user Id
+  // outgoing: success or error
+
+  try {
+    const db = client.db("Overcastly");
+    let _id = req.params._id;
+
+    let delResult = await db.collection("Users").deleteOne({ _id: new ObjectId(_id) });
+
+    if (delResult.deletedCount === 0) {
+      return res.status(404).json({ error: "nothing deleted, user prolly doesnt exist" });
+    }
+
+    return res.status(200).json({ message: "BOOM! get DUHLEETED" });
+
+  } catch (e) {
+    return res.status(500).json({ error: "Couldnt delete that user idk why..." });
+  }
 });
 
 app.use((req, res, next) => {
